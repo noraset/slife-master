77fcb8e77d44e11dee203d00b34b1ee4ed6abd06
DESKTOP-MJQDJKD\User <noreset1994@gmail.com>
2018-07-16 02:39:45 +0700

    add filter

--- app/src/main/java/com/singhaestate/slife/ui/mainmenu/buildinginfo/MapsActivity.kt
+++ app/src/main/java/com/singhaestate/slife/ui/mainmenu/buildinginfo/MapsActivity.kt
@@ -1,17 +1,25 @@
 package com.singhaestate.slife.ui.mainmenu.buildinginfo
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.Marker
 import com.singhaestate.slife.R
 import com.singhaestate.slife.ui.mainmenu.shoplist.search.SearchActivity
+import com.singhaestate.slife.util.Contextor.context
 import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
@@ -21,6 +29,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
     private lateinit var mMap: GoogleMap
     private lateinit var lastLocation: Location
+    private val mMenus: MutableList<FilterItem> = arrayListOf()
+    private var mAdapter: FilterAdapter? = null
+    private var isSelectAll: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -29,6 +41,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         init()
+
     }
 
     /**
@@ -53,13 +66,56 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     }
 
     private fun init() {
+        mMenus.add(FilterItem("", "สแกนบาร์โค้ด", false))
+        mMenus.add(FilterItem("", "สแกนบาร์โค้ด", false))
+        mMenus.add(FilterItem("", "สแกนบาร์โค้ด", false))
+        mMenus.add(FilterItem("", "สแกนบาร์โค้ด", false))
+        mMenus.add(FilterItem("", "สแกนบาร์โค้ด", false))
+        mMenus.add(FilterItem("", "สแกนบาร์โค้ด", false))
+        initMenu()
         rootLayout.requestFocus()
         toolbar.setNavigationIcon(R.drawable.ic_back_white)
         toolbar.setNavigationOnClickListener { finish() }
         toolbar_title.text = "Building Info"
+        toolbarFilter.setNavigationIcon(R.drawable.ic_back_blue)
+        toolbarFilter.setNavigationOnClickListener {
+            closeFilter()
+        }
+        toolbar_title_filter.text = "Filter"
         search_bar.setOnClickListener {
             startActivity(Intent(this, SearchActivity::class.java))
         }
+        btn_filter.setOnClickListener {
+            openFilter()
+        }
+        btn_search.setOnClickListener {
+            closeFilterAndSearch()
+        }
+        btn_select.setOnClickListener {
+            if (isSelectAll) {
+                unSelectAll()
+            } else {
+                selectAll()
+            }
+        }
+    }
+
+    private fun closeFilterAndSearch() {
+        view_overlay_filter.visibility = View.GONE
+    }
+
+    private fun closeFilter() {
+        view_overlay_filter.visibility = View.GONE
+    }
+
+    private fun openFilter() {
+        view_overlay_filter.visibility = View.VISIBLE
+    }
+
+
+    private fun initMenu() {
+        mAdapter = FilterAdapter()
+        listView.adapter = mAdapter
     }
 
     companion object {
@@ -77,5 +133,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         mMap.isMyLocationEnabled = true
 // 2
     }
+
+    inner class FilterAdapter : BaseAdapter() {
+
+        @SuppressLint("ResourceAsColor")
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+            val view = convertView
+                    ?: LayoutInflater.from(context).inflate(R.layout.filter_item, parent, false)
+
+            val item = mMenus[position]
+            val title = view.findViewById<TextView>(R.id.title)
+            val icon = view.findViewById<ImageView>(R.id.icon)
+            title.text = item.name
+            if (mMenus[position].isSelect) {
+                icon.setImageResource(R.drawable.barcode_icon)
+            } else {
+                icon.setImageResource(R.drawable.activity_icon)
+            }
+            view.setOnClickListener {
+                select(position)
+            }
+
+            return view
+        }
+
+        override fun getItem(position: Int): Any = mMenus[position]
+        override fun getItemId(position: Int): Long = 0L
+        override fun getCount(): Int = mMenus.size
+
+    }
+
+    private fun select(position: Int) {
+        mMenus[position].isSelect = !mMenus[position].isSelect
+        mAdapter!!.notifyDataSetChanged()
+    }
+
+    private fun selectAll() {
+        for (i in 0..(mMenus.size - 1)) {
+            mMenus[i].isSelect = true
+        }
+        isSelectAll = true
+        mAdapter!!.notifyDataSetChanged()
+        btn_select.text = "UnSeclect All"
+    }
+
+    private fun unSelectAll() {
+        for (i in 0..(mMenus.size - 1)) {
+            mMenus[i].isSelect = false
+        }
+        isSelectAll = false
+        mAdapter!!.notifyDataSetChanged()
+        btn_select.text = "Seclect All"
+    }
+
+    data class FilterItem(
+            val icon: String,
+            val name: String,
+            var isSelect: Boolean
+    )
 
 }
